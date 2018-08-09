ae250cf56c450216ba39cef0076bd2221219451f
Fahmi Akbar Wildana <fahmi.akbar.w@mail.ugm.ac.id>
2018-08-10 00:19:24 +0700

    chore(lint): add static typing using flow

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+// @flow
+
+const pi = (s: number) => s * 3.14;
+
 export default function() {
-  const a = { sd: '1' };
+  const a = { sd: pi(2) };
   while (process.env.NODE) {
     console.log('hello world!!', a);
   }
