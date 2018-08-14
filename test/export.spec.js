e4468ae002680a35900ad8eefee5cf9979e353c1
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-08-15 05:13:13 +0700

    test(export): add case for several export option
    ❌ silent warning message when generating bundle
    ✔️ [temp] all postive case will match empty object

--- test/export.spec.js
+++ test/export.spec.js
@@ -3,19 +3,65 @@ import { rollup } from 'rollup';
 import { require } from './util';
 import rust from '../src/main.js';
 
-test('test export', async () => {
-  const bundle = await rollup({
-    input: 'test/fixtures/single_function/index.js',
-    plugins: [rust()]
+describe('test export option', () => {
+  const input = 'test/fixtures/single_function/index.js';
+
+  test('export as Buffer class', async () => {
+    const bundle = await rollup({
+      input,
+      plugins: [rust({ export: 'buffer' })]
+    });
+
+    const wasmCode = await require(bundle);
+
+    // Usage
+    const module = new WebAssembly.Module(wasmCode);
+    const instance = new WebAssembly.Instance(module);
+
+    expect(wasmCode).toBeInstanceOf(Buffer);
+    expect(instance.exports).toMatchObject({});
   });
 
-  const wasmCode = await require(bundle);
+  test('export as WebAssembly.Module', async () => {
+    const bundle = await rollup({
+      input,
+      plugins: [rust({ export: 'module' })]
+    });
 
-  // Usage
-  const module = new WebAssembly.Module(wasmCode);
-  const instance = new WebAssembly.Instance(module);
+    const wasmModule = await require(bundle);
 
-  expect(wasmCode).toBeInstanceOf(Buffer);
-  expect(module).toBeInstanceOf(WebAssembly.Module);
-  expect(instance).toBeInstanceOf(WebAssembly.Instance);
+    // Usage
+    const instance = new WebAssembly.Instance(wasmModule);
+
+    expect(wasmModule).toBeInstanceOf(WebAssembly.Module);
+    expect(instance.exports).toMatchObject({});
+  });
+
+  test('export as WebAssembly.Instance', async () => {
+    const bundle = await rollup({
+      input,
+      plugins: [rust({ export: 'instance' })]
+    });
+
+    const wasmInstance = await require(bundle);
+
+    expect(wasmInstance).toBeInstanceOf(WebAssembly.Instance);
+    expect(wasmInstance.exports).toMatchObject({}); // can be used directly, not suitable for big size wasm
+  });
+
+  test('export of WebAssembly.instantiate', async () => {
+    const bundle = await rollup({
+      input,
+      plugins: [rust({ export: 'promise' })]
+    });
+
+    const compileWasm = await require(bundle);
+
+    // Usage
+    const { module, instance } = await compileWasm;
+
+    expect(module).toBeInstanceOf(WebAssembly.Module);
+    expect(instance).toBeInstanceOf(WebAssembly.Instance);
+    expect(instance.exports).toMatchObject({});
+  });
 });
--- test/fail.spec.js
+++ test/fail.spec.js
@@ -2,12 +2,26 @@ import { rollup } from 'rollup';
 import { require } from './util';
 import rust from '../src/main.js';
 
-test('wrong input', async () => {
-  const bundle = await rollup({
-    input: 'test/fixtures/empty.js',
-    plugins: [rust()]
+describe('negative test', () => {
+  test('wrong input', async () => {
+    const bundle = await rollup({
+      input: 'test/fixtures/empty.js',
+      plugins: [rust({ export: 'buffer' })]
+    });
+
+    global.console.warn = jest.fn(); // probably rollup give warning when generate bundle
+    const wasmCode = await require(bundle);
+
+    expect(wasmCode).not.toBeInstanceOf(Buffer);
+    expect(console.warn).toHaveBeenCalled();
   });
 
-  const wasmCode = await require(bundle);
-  expect(wasmCode).not.toBeInstanceOf(Buffer);
+  test('wrong export option', async () => {
+    const tryToRollup = rollup({
+      input: 'test/fixtures/single_function/index.js',
+      plugins: [rust({ export: 'neon' })]
+    });
+
+    await expect(tryToRollup).rejects.toBeInstanceOf(Error);
+  });
 });
