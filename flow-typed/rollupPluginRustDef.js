7830335bbfd216c354ea2c7206257ab8a0ce63aa
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-08-14 20:04:37 +0700

    feat(export): add option to export wasm code as X
    X can be
    🐣 'buffer' will export wasm code as Buffer class
    🐥 'module' will export wasm code as WebAssembly.Module
    🐤 'instance' will export wasm code as WebAssembly.Instance
    🐔 'promise' will "instantiate" wasm code asynchronously
    
    first attempt, no test yet, so [skip ci]

--- flow-typed/rollupPluginRustDef.js
+++ flow-typed/rollupPluginRustDef.js
@@ -1,4 +1,5 @@
 declare type Options = {
+  export: 'buffer' | 'instance' | 'module' | 'promise',
   include?: string[] | string, // rollup recommend to add this
   exclude?: string[] | string // rollup recommend to add this
 };
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 // @flow
 import { createFilter } from 'rollup-pluginutils';
+import wrap from './wrapper';
 import predef from './options';
 
 const extension = /\.rs$/; // rust file extension
 
 export default function(options: Options = predef) {
-  options = Object.assign(options, predef); // diff and merge with predefined options
+  options = Object.assign(predef, options); // diff and merge with predefined options
   const filter = createFilter(options.include, options.exclude);
 
   return {
@@ -14,17 +15,17 @@ export default function(options: Options = predef) {
       if (!extension.test(id)) return;
       if (!filter(id)) return;
 
-      const wasmCode = Buffer.from([
-        0x00,
-        0x61,
-        0x73,
-        0x6d,
-        0x01,
-        0,
-        0,
-        0
-      ]).toJSON().data;
-      return `export default Buffer.from([${wasmCode.toString()}])`;
+      const wasmCode = Buffer.from([0x00, 0x61, 0x73, 0x6d, 0x01, 0, 0, 0]);
+      switch (options.export) {
+        case 'buffer':
+          return wrap(wasmCode).asBuffer;
+        case 'instance':
+          return wrap(wasmCode).asWebAssembly.Instance;
+        case 'module':
+          return wrap(wasmCode).asWebAssembly.Module;
+        case 'promise':
+          return wrap(wasmCode).promiseWebAssembly;
+      }
     }
   };
 }
