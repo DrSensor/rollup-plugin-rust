f2664dceddb4e5cb085e6003839e8fe2395bb8f2
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-08-15 17:46:21 +0700

    feat(cargo): use cargo to build rust into wasm
    ㊙️ under the hood, it use rust-native-wasm-loader
    ✍️ use $Shape to relax Options type
    👫 partially polyfill webpack specific context
    
    [ci skip]

--- flow-typed/rollupPluginRustDef.js
+++ flow-typed/rollupPluginRustDef.js
@@ -1,5 +1,7 @@
 declare type Options = {
+  target: string,
+  release: boolean,
   export: 'buffer' | 'instance' | 'module' | 'promise',
-  include?: string[] | string, // rollup recommend to add this
-  exclude?: string[] | string // rollup recommend to add this
+  include: string[] | string, // rollup recommend to add this
+  exclude: string[] | string // rollup recommend to add this
 };
