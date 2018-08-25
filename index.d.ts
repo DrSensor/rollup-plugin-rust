48a168d9f1781abbf14b0b2f3072d90ed5844b18
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-08-25 09:48:09 +0700

    chore(type): add typedef for async* options
    💔 deprecate `promise` in favor of `async`
    
    [ci skip]

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,13 @@
 interface Options {
   target: string; // use by rust-native-wasm-loader
   release: boolean; // use by rust-native-wasm-loader
-  export: "buffer" | "instance" | "module" | "promise";
+  export:
+    | "buffer"
+    | "instance"
+    | "module"
+    | "async"
+    | "async-instance"
+    | "async-module";
   include: string[] | string; // rollup recommend to add this
   exclude: string[] | string; // rollup recommend to add this
 }
--- src/options.js
+++ src/options.js
@@ -3,7 +3,7 @@
 export default ({
   target: 'wasm32-unknown-unknown',
   release: true,
-  export: 'promise',
+  export: 'async',
   include: ['**/*.rs'],
   exclude: ['node_modules/**', 'target/**']
 }: Options);
